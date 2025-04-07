4b836ba9c02926e9881bdbc736bb1b4155c1df57
MrXnneHang <XnneHang@gmail.com>
2025-04-07 09:37:43 +0800

    :white_check_mark: test: Testing for CommandGenerater.gen_args.

--- src/uiya/test.py
+++ src/uiya/test.py
@@ -9,8 +9,25 @@ if TYPE_CHECKING:
 
 
 def main():
+    print("================= Testing CommandGenerater Init=================")
+    # from values
+    command_generator = CommandGenerator(
+        target_type="video",
+        batch_download=False,
+        support_select=False,
+        url="https://example.com/video123",  # adjustable
+        selected_p=None,  # adjustable/Optional
+        require_video=True,  # adjustable
+        require_audio=True,  # adjustable
+        require_danmaku=False,  # adjustable
+        require_cover=False,  # adjustable
+        debug_mode=False,  # adjustable
+        video_quality="360p 流畅",  # adjustable
+        audio_quality="320kbps",  # adjustable
+    )
+    print(command_generator)
 
-    print("================= Testing CommandGenerater =================")
+    # from status
     status: CommandStatus = {
         "target_type": "video",
         "batch_download": False,
@@ -29,6 +46,11 @@ def main():
     command_generator = CommandGenerator.from_status(status)
     # 打印实例的属性
     print(command_generator)
+
+    print("================= Testing CommandGenerater get_args=================")
+    command_generator = CommandGenerator.from_status(status)
+    command_args = command_generator.gen_args()
+    print(command_args)
 
 
 if __name__ == "__main__":
